9cef0d51efcb76bdd4f152cd9a01af44228ded7b
RayaFarshad <rayafarshad@gmail.com>
2020-03-12 16:00:03 -0700

    fix dashboard

--- application/src/routes/dashboardRoutes.js
+++ application/src/routes/dashboardRoutes.js
@@ -2,17 +2,6 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models/database.js");
 
-// async function getUserInfoFromRatings(req, res, next){
-//     let userID = req.user.user_id;
-//     let query = " SELECT rate_id FROM db.ratings where users_user_id = "+userID;
-//     console.log(query);
-//     await db.execute(query , (err, ratings) => {
-        
-//         if(err) throw err;
-//         req.ratings = ratings;
-//         next();
-//     });
-// }
 async function getUserInfoFromRatings(req, res, next){
     let userID = req.user.user_id;
     let query = " SELECT * FROM db.ratings R, db.captions C, db.images I where R.captions_cap_id = C.cap_id AND C.images_img_id = I.img_id AND R.users_user_id = "+userID;
@@ -26,28 +15,30 @@ async function getUserInfoFromRatings(req, res, next){
 }
 
 
-
-// async function getCaptions(req, res, next){
-//     let query = " SELECT cap_id as cap_id FROM db.captions c where c.cap_id IN (SELECT captions_cap_id from db.ratings r where r.users_user_id = "+ req.user.user_id +" AND r.captions_cap_id = c.cap_id) ";
-//     console.log(query);
-//     await db.execute(query , (err, captions) => {
+async function getUserInfo(req, res, next){
+    let query = " SELECT * FROM db.users where user_id = "+ req.user.user_id;
+    console.log(query);
+    await db.execute(query , (err, users) => {
         
-//         if(err) throw err;
-//         req.captions = captions;
-//         next();
-//     });
-// }
+        if(err) throw err;
+        req.users = users;
+        next();
+    });
+}
 
 
 
 
-router.get("/dashboard", getUserInfoFromRatings, function(req, res, next) {
+router.get("/dashboard", getUserInfoFromRatings, getUserInfo,  function(req, res, next) {
     // console.log(ratings);
     let ratings = req.ratings;
-    console.log(ratings);
-    console.log(ratings[0].caption);
+    let users = req.users;
+    // console.log(ratings);
+    console.log("users: "+users);
+    // console.log(ratings[0].caption);
     res.render("dashboard", {
         ratings : ratings,
+        users : users,
     });
   });
 
