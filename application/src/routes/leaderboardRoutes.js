c5e1c4a11fd202c34351beb4db4c3c028afb1f0c
Jose C <jcastan6@mail.sfsu.edu>
2020-06-17 12:17:07 -0700

    changed leaderboard display

--- application/src/routes/leaderboardRoutes.js
+++ application/src/routes/leaderboardRoutes.js
@@ -1,56 +1,60 @@
-
 const express = require("express");
 const router = express.Router();
 const db = require("../models/database.js");
 
+async function sortByTotalScore(req, res, next) {
+  // rate = req.body.inlineRadioOptions;
+  let query =
+    " SELECT * FROM db.users  where id not in (21,22) order by total_score desc LIMIT 50 ";
+  console.log(query);
+  await db.execute(query, (err, sortByTotalScore) => {
+    if (err) throw err;
+    req.sortByTotalScore = sortByTotalScore;
 
-async function sortByTotalScore(req,res,next){
-    // rate = req.body.inlineRadioOptions;
-    let query = " SELECT * FROM db.users  where id not in (21,22) order by total_score desc LIMIT 5 ";
-    console.log(query);
-    await db.execute(query, (err, sortByTotalScore) => {
-        if(err) throw err;
-        req.sortByTotalScore = sortByTotalScore;
-
-        next();
-    });
+    next();
+  });
 }
-async function sortByAccuracy(req,res,next){
-    // rate = req.body.inlineRadioOptions;
-    let query = " SELECT * FROM db.users  where id not in (21,22) order by level desc LIMIT 5 ";
-    console.log(query);
-    await db.execute(query, (err, sortByAccuracy) => {
-        if(err) throw err;
-        req.sortByAccuracy = sortByAccuracy;
+async function sortByAccuracy(req, res, next) {
+  // rate = req.body.inlineRadioOptions;
+  let query =
+    " SELECT * FROM db.users  where id not in (21,22) order by level desc LIMIT 50 ";
+  console.log(query);
+  await db.execute(query, (err, sortByAccuracy) => {
+    if (err) throw err;
+    req.sortByAccuracy = sortByAccuracy;
 
-        next();
-    });
+    next();
+  });
 }
-async function sortByScoreAccuracy(req,res,next){
-    // rate = req.body.inlineRadioOptions;
-    let query = " SELECT *, (total_score*level)/100 as result  FROM db.users  where id not in (21,22) order by result desc LIMIT 5 ";
-    console.log(query);
-    await db.execute(query, (err, sortByScoreAccuracy) => {
-        if(err) throw err;
-        req.sortByScoreAccuracy = sortByScoreAccuracy;
+async function sortByScoreAccuracy(req, res, next) {
+  // rate = req.body.inlineRadioOptions;
+  let query =
+    " SELECT *, (total_score*level)/100 as result  FROM db.users  where id not in (21,22) order by result desc LIMIT 50 ";
+  console.log(query);
+  await db.execute(query, (err, sortByScoreAccuracy) => {
+    if (err) throw err;
+    req.sortByScoreAccuracy = sortByScoreAccuracy;
 
-        next();
-    });
+    next();
+  });
 }
 
-  router.get("/leaderboard", sortByTotalScore, sortByAccuracy,sortByScoreAccuracy,function(req, res, next) {
-
+router.get(
+  "/leaderboard",
+  sortByTotalScore,
+  sortByAccuracy,
+  sortByScoreAccuracy,
+  function(req, res, next) {
     let sort_TotalScore = req.sortByTotalScore;
     let sort_ByAccuracy = req.sortByAccuracy;
     let sort_ByScoreAccuracy = req.sortByScoreAccuracy;
 
     res.render("leaderboard", {
-        sort_TotalScore : sort_TotalScore,
-        sort_ByAccuracy: sort_ByAccuracy,
-        sort_ByScoreAccuracy : sort_ByScoreAccuracy,
-
+      sort_TotalScore: sort_TotalScore,
+      sort_ByAccuracy: sort_ByAccuracy,
+      sort_ByScoreAccuracy: sort_ByScoreAccuracy
     });
-  });
+  }
+);
 
-
-  module.exports = router;
+module.exports = router;
