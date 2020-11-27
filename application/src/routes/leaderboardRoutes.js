ab00f2c095f7a8933761fb5f32da4ac7ccbd5798
jcastan6 <josecastanon4@gmail.com>
2020-11-27 12:39:52 -0800

    test

--- application/src/routes/leaderboardRoutes.js
+++ application/src/routes/leaderboardRoutes.js
@@ -7,7 +7,7 @@ async function sortByTotalScore(req, res, next) {
   let query =
     "SELECT * FROM db.users  where id not in (21,22) order by total_score desc LIMIT 50;";
   console.log(query);
-  await db.execute(query, (err, sortByTotalScore) => {
+  db.execute(query, (err, sortByTotalScore) => {
     //if (err) throw err;
     req.sortByTotalScore = sortByTotalScore;
 
@@ -19,7 +19,7 @@ async function sortByAccuracy(req, res, next) {
   let query =
     "SELECT * FROM db.users  where id not in (21,22) order by level desc LIMIT 50;";
   console.log(query);
-  await db.execute(query, (err, sortByAccuracy) => {
+  db.execute(query, (err, sortByAccuracy) => {
     //if (err) throw err;
     req.sortByAccuracy = sortByAccuracy;
 
@@ -31,7 +31,7 @@ async function sortByScoreAccuracy(req, res, next) {
   let query =
     "SELECT *, (total_score*level)/100 as result  FROM db.users  where id not in (21,22) order by result desc LIMIT 50;";
   console.log(query);
-  await db.execute(query, (err, sortByScoreAccuracy) => {
+  db.execute(query, (err, sortByScoreAccuracy) => {
     //if (err) throw err;
     req.sortByScoreAccuracy = sortByScoreAccuracy;
 
