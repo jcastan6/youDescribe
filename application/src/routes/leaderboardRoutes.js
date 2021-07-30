97415bfcbec27c2e7a19b156edf2c28cdd3a5c16
jcastan6 <josecastanon4@gmail.com>
2021-07-29 17:20:59 -0700

    updated leaderboard queries

--- application/src/routes/leaderboardRoutes.js
+++ application/src/routes/leaderboardRoutes.js
@@ -12,41 +12,31 @@ async function sortByTotalScore(req, res, next) {
   req.sortByTotalScore = result[0];
   next();
 }
-async function sortByAccuracy(req, res, next) {
+
+async function sortByDailyScore(req, res, next) {
   // rate = req.body.inlineRadioOptions;
+  //selects user with highest points in last 24 hours
   let query =
-    "SELECT * FROM captionrater.users  where id not in (21,22) order by level desc LIMIT 5";
+    "SELECT user.username, sum(scores) as result FROM ratings INNER JOIN users user ON users_user_id = user.id  WHERE ratings.createdAt > DATE_SUB(CURDATE(), INTERVAL 2 DAY) GROUP by users_user_id order by result DESC LIMIT 3;";
   console.log(query);
   const result = await db.query(query);
   console.log(result);
-  req.sortByAccuracy = result[0];
-  next();
-}
-async function sortByScoreAccuracy(req, res, next) {
-  // rate = req.body.inlineRadioOptions;
-  let query =
-    "SELECT *, (total_score*level)/100 as result  FROM captionrater.users  where id not in (21,22) order by result desc LIMIT 5";
-  console.log(query);
-  const result = await db.query(query);
-  console.log(result);
-  req.sortByScoreAccuracy = result[0];
+  req.sortByDailyScore = result[0];
   next();
 }
 
 router.get(
   "/leaderboard",
   sortByTotalScore,
-  sortByAccuracy,
-  sortByScoreAccuracy,
+
+  sortByDailyScore,
   function (req, res, next) {
     let sort_TotalScore = req.sortByTotalScore;
-    let sort_ByAccuracy = req.sortByAccuracy;
-    let sort_ByScoreAccuracy = req.sortByScoreAccuracy;
+    let sort_ByDailyScore = req.sortByDailyScore;
 
     res.render("leaderboard", {
       sort_TotalScore: sort_TotalScore,
-      sort_ByAccuracy: sort_ByAccuracy,
-      sort_ByScoreAccuracy: sort_ByScoreAccuracy,
+      sort_ByDailyScore: sort_ByDailyScore,
     });
   }
 );
